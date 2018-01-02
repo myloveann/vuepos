9257e72e9e2d78aad332b9cbc4f3070536031b04
myloveann <rj14108@163.com>
2018-01-02 17:22:25 +0800

    add the left layout

--- vuepos/src/components/page/pos.vue
+++ vuepos/src/components/page/pos.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="pos">
     <el-row>
-      <el-col :span="7" class="pos-order" id="order-list">我是订单栏</el-col>
+      <el-col :span="7" class="pos-order" id="order-list">
+        <el-tabs>
+          <el-tab-pane label="点餐">
+            <el-table :data="tableData" border style="width:100%">
+              <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+              <el-table-column prop="count" label="数量" width="50"></el-table-column>
+              <el-table-column prop="price" label="金额" width="70"></el-table-column>
+              <el-table-column prop="caozuo" label="操作" width="100"  fixed="right">
+                <template slot-scope="scope">
+                  <el-button type="text" size="samll">删除</el-button>
+                  <el-button type="text" size="samll">增加</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <div class="div-btn">
+              <el-button type="warning">挂单</el-button>
+              <el-button type="danger">删除</el-button>
+              <el-button type="success">结账</el-button>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="挂单">挂单</el-tab-pane>
+          <el-tab-pane label="外卖">外卖</el-tab-pane>
+        </el-tabs>
+
+      </el-col>
       <el-col :span="17">我是产品栏</el-col>
     </el-row>
   </div>
@@ -12,12 +36,34 @@ export default {
   name: 'pos',
   data () {
     return {
-      msg: 'Hello AwesomePos demo'
+      tableData:[
+        {
+          goodsName:'可口可乐',
+          price:8,
+          count:1
+        },
+        {
+          goodsName:'可口可乐',
+          price:8,
+          count:1
+        },
+        {
+          goodsName:'可口可乐',
+          price:8,
+          count:1
+        },
+        {
+          goodsName:'可口可乐',
+          price:8,
+          count:1
+        },
+      
+      ]
     }
   },
   mounted:function(){
     var orderHeight=document.body.clientHeight;
-    document.getElementById('order-list').style.height=orderHeight+'px'
+    document.getElementById('order-list').style.height=orderHeight+'px';
   }
 }
 </script>
@@ -29,4 +75,9 @@ export default {
   border-right: 1px solid #C0CCDA;
 
 }
+.div-btn{
+  margin-top: 10px;
+}
+
+
 </style>
